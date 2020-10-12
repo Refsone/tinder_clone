3056c2a7be458ee545ac80e32253ec8e88a70a50
MANAUT <tristanmanaut@gmail.com>
2020-10-12 09:15:37 +0200

    add card and swipe cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import TinderCard from 'react-tinder-card';
 
 import './App.css';
 import Header from './Header';
+import TinderCards from './TinderCards';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <TinderCards />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import PersonIcon from '@material-ui/icons/Person'
+import IconButton from '@material-ui/core/IconButton'
+import ForumIcon from '@material-ui/icons/Forum'
+
 import './Header.css'
 
 const Header = () => {
     return (
         <div className="header">
-            <h1>I am a header !</h1>
+            <IconButton>
+                <PersonIcon fontSize="large" className="header__icon"/>
+            </IconButton>
+            <img 
+                className="header__logo"
+                src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+                alt=""
+            /> 
+            <IconButton>
+                <ForumIcon fontSize="large" className="header__icon"/>
+            </IconButton>
         </div>
     )
 }
